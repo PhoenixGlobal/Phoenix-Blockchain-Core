f4106e924a25cfa569eb43b770da9709cbd27591
xiaorui <ruixiao.xiao@chinapex.com>
2018-12-24 16:02:44 +0800

    update transaction receipt

--- src/main/scala/com/apex/core/TransactionReceipt.scala
+++ src/main/scala/com/apex/core/TransactionReceipt.scala
@@ -11,12 +11,14 @@
 package com.apex.core
 import java.io.{DataInputStream, DataOutputStream}
 
-import com.apex.crypto.UInt256
+import com.apex.crypto.{UInt160, UInt256}
 
-case class TransactionReceipt(txId: UInt256, gasUsed: Long, totalGasUsed: Long, output: Array[Byte], status: Int) extends com.apex.common.Serializable {
+case class TransactionReceipt(txId: UInt256, from: UInt160, to: UInt160, gasUsed: Long, totalGasUsed: Long, output: Array[Byte], status: Int) extends com.apex.common.Serializable {
   override def serialize(os: DataOutputStream): Unit = {
     import com.apex.common.Serializable._
     os.write(txId)
+    os.write(from)
+    os.write(to)
     os.writeLong(gasUsed)
     os.writeLong(totalGasUsed)
     os.writeByteArray(output)
@@ -29,6 +31,8 @@ object TransactionReceipt {
     import com.apex.common.Serializable._
     TransactionReceipt(
       is.readObj[UInt256],
+      is.readObj[UInt160],
+      is.readObj[UInt160],
       is.readLong,
       is.readLong,
       is.readByteArray,
