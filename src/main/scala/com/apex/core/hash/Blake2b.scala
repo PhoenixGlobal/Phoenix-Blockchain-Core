828e8c8cedc8ea9b2ed9a353145eeff3fd88aa4a
xiaorui <ruixiao.xiao@chinapex.com>
2019-01-08 17:35:28 +0800

    delete unused codes

--- src/main/scala/com/apex/core/hash/Blake2b.scala
+++
@@ -1,8 +0,0 @@
-package com.apex.core.hash
-
-import org.bouncycastle.crypto.digests.Blake2bDigest
-
-
-trait Blake2b[D <: Digest] extends BouncyCastleHash[D] {
-  override protected lazy val digestFn = new Blake2bDigest(DigestSize * 8)
-}
