6c71dc2655f3d3f9c84ff182e1aa84d19b3e7fd1
whitney.wei <whitney.wei@chinapex.com>
2019-03-22 17:48:51 +0800

    gas average

---
+++ src/main/scala/com/apex/plugins/mongoprice/GasPricePlugin.scala
@@ -0,0 +1,68 @@
+package com.apex.plugins.mongodb
+
+import akka.actor.{Actor, ActorContext, ActorRef, Props}
+import com.apex.common.ApexLogging
+import com.apex.core._
+import com.apex.crypto.FixedNumber
+import com.apex.rpc.GetAverageCmd
+import com.apex.settings.ApexSettings
+import scala.collection.mutable
+import scala.concurrent.ExecutionContext
+
+class GasPricePlugin(settings: ApexSettings)
+                   (implicit ec: ExecutionContext) extends Actor with ApexLogging {
+
+  private val blocks = mutable.Queue[Block]()
+  private var averageValue:FixedNumber = FixedNumber.Zero
+
+  override def postStop(): Unit = {
+    log.info("mongodb plugin stopped")
+    super.postStop()
+  }
+
+  override def receive: Receive = {
+    case BlockAddedToHeadNotify(blockSummary) => {
+      if(blocks.size > 1000) blocks.dequeue
+      blocks.enqueue(blockSummary.block)
+      calculateAverage()
+    }
+    case GetAverageCmd =>{
+      sender() ! averageValue.toString
+    }
+    case a: Any => {
+      log.info(s"${sender().toString}, ${a.toString}")
+    }
+  }
+
+  def calculateAverage(): Unit ={
+    var sumPrice:FixedNumber = FixedNumber.Zero
+    var sumTx:BigInt = 0
+    blocks.foreach(block => {
+      block.transactions.foreach(tx =>{
+        sumPrice += tx.gasPrice
+        sumTx += 1
+      })
+    })
+
+    if(sumTx >0 ) averageValue = sumPrice/sumTx
+  }
+
+}
+
+object GasPricePluginRef {
+  def props(settings: ApexSettings)
+           (implicit ec: ExecutionContext): Props = {
+    Props(new GasPricePlugin(settings))
+  }
+
+  def apply(settings: ApexSettings)
+           (implicit system: ActorContext, ec: ExecutionContext): ActorRef = {
+    system.actorOf(props(settings))
+  }
+
+  def apply(settings: ApexSettings,
+            name: String)
+           (implicit system: ActorContext, ec: ExecutionContext): ActorRef = {
+    system.actorOf(props(settings), name)
+  }
+}
