0bd4b25dab7fc3decc05f27901062a496f05f076
Shan Huang <shan.huang@chinapex.com>
2019-04-19 18:06:53 +0800

    proposal db

--- src/main/scala/com/apex/core/DataBase.scala
+++ src/main/scala/com/apex/core/DataBase.scala
@@ -13,8 +13,9 @@ package com.apex.core
 import java.time.Instant
 
 import com.apex.common.ApexLogging
-import com.apex.consensus.{WitnessVote, WitnessInfo, WitnessList, WitnessMap}
+import com.apex.consensus.{WitnessInfo, WitnessList, WitnessMap, WitnessVote}
 import com.apex.crypto.{BinaryData, FixedNumber, UInt160, UInt256}
+import com.apex.proposal.{Proposal, ProposalVoteList}
 import com.apex.settings.DataBaseSettings
 import com.apex.storage.Storage
 import com.apex.vm.DataWord
@@ -28,7 +29,6 @@ class DataBase(settings: DataBaseSettings, db: Storage.lowLevelRaw, tracking: Tr
   private val receiptStore = new ReceiptStore(tracking, settings.cacheSize)
   private val contractStore = new ContractStore(tracking, settings.cacheSize)
   private val contractStateStore = new ContractStateStore(tracking, settings.cacheSize)
-  private val nameToAccountStore = new NameToAccountStore(tracking, settings.cacheSize)
   private val scheduleTxStore = new ScheduleTxStore(tracking, settings.cacheSize)
 
   private val witnessVoteStore = new WitnessVoteStore(tracking, settings.cacheSize)
@@ -37,6 +37,9 @@ class DataBase(settings: DataBaseSettings, db: Storage.lowLevelRaw, tracking: Tr
   private val previousWitnessStore = new PreviousWitnessStore(tracking)
   private val currentWitnessStore = new CurrentWitnessStore(tracking)
   private val pendingWitnessStore = new PendingWitnessStore(tracking)
+
+  private val proposalStore = new ProposalStore(tracking, settings.cacheSize)
+  private val proposalVoteListStore = new ProposalVoteListStore(tracking)
 
   def this(settings: DataBaseSettings, db: Storage.lowLevelRaw) = {
     this(settings, db, Tracking.root(db))
@@ -194,6 +197,9 @@ class DataBase(settings: DataBaseSettings, db: Storage.lowLevelRaw, tracking: Tr
   // next active producer
   def getPendingWitnessList(): Option[WitnessList] = pendingWitnessStore.get()
   def setPendingWitnessList(wl: WitnessList): Unit = pendingWitnessStore.set(wl)
+
+  def getProposal(proposalID: UInt256): Option[Proposal] = proposalStore.get(proposalID)
+  def getProposalVoteList(): Option[ProposalVoteList] = proposalVoteListStore.get
 
 
   def startTracking(): DataBase = {
--- src/main/scala/com/apex/proposal/ProposalVote.scala
+++ src/main/scala/com/apex/proposal/ProposalVote.scala
@@ -5,30 +5,33 @@ import java.io.{DataInputStream, DataOutputStream}
 import com.apex.common.Serializable
 import com.apex.crypto.{UInt160, UInt256}
 
-case class ProposalVoteData(proposalID: UInt256,
-                            agree: Boolean,
-                            version: Int = 0x01) extends Serializable {
+case class ProposalVote(proposalID: UInt256,
+                        voter: UInt160,
+                        agree: Boolean,
+                        version: Int = 0x01) extends Serializable {
 
   override def serialize(os: DataOutputStream): Unit = {
     import com.apex.common.Serializable._
 
     os.writeInt(version)
     os.write(proposalID)
+    os.write(voter)
     os.writeBoolean(agree)
   }
 
 }
 
-object ProposalVoteData {
+object ProposalVote {
 
-  def deserialize(is: DataInputStream): ProposalVoteData = {
+  def deserialize(is: DataInputStream): ProposalVote = {
     import com.apex.common.Serializable._
 
     val version = is.readInt()
     val proposalID = UInt256.deserialize(is)
+    val voter = UInt160.deserialize(is)
     val agree = is.readBoolean()
 
-    new ProposalVoteData(proposalID, agree, version)
+    new ProposalVote(proposalID, voter, agree, version)
   }
 
 }
